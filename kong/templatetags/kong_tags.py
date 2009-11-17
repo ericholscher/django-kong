0c9f47fefc1250b23fcc52e06bc1e96fabd68d5d
Eric Holscher <eric@ericholscher.com>
2009-11-16 23:09:22 -0600

    Added a render filter for results.

--- kong/templatetags/kong_tags.py
+++ kong/templatetags/kong_tags.py
@@ -2,7 +2,10 @@ from django import template
 
 register = template.Library()
 
+@register.filter
 def micro_to_milli(value):
     return value/1000
 
-register.filter('micro_to_milli', micro_to_milli)
+@register.filter
+def render_twill(result):
+    return result.test.render(result.site)
