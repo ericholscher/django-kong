bd565308f1187312f34a5dee6acd027cae072248
Eric Holscher <eric@ericholscher.com>
2009-11-16 22:52:01 -0600

    Remove a bunch of cruft from the sites. Now it just has the latest view and the test history for a site. Will add other views once I know what to add, isntead of just having a bunch that seem like a good idea.

--- kong/urls.py
+++ kong/urls.py
@@ -6,23 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
      (r'^admin/doc/', include('django.contrib.admindocs.urls')),
      (r'^admin/', include(admin.site.urls)),
-
-     url(r'^$', direct_to_template, {'template': 'index.html'}, name='kong_home'),
-     url(r'^index/$', 'kong.views.index', name='kong_index'),
-
-     url(r'^sites/(?P<site_slug>.*?)/', 'kong.views.site_object', name='kong_site_detail'),
-     url(r'^sites/', 'kong.views.site_list', name='kong_site_list'),
-
-     url(r'^tests/(?P<id>\d+)/', 'kong.views.testresult_detail', name='kong_testresult_detail'),
+     url(r'^$', 'kong.views.index', name='kong_index'),
+     url(r'index/$', 'kong.views.index', name='kong_index'),
      url(r'^tests/(?P<test_slug>.*?)/(?P<site_slug>.*?)/', 'kong.views.test_object_for_site', name='kong_testresult_for_site'),
-     url(r'^tests/(?P<test_slug>.*?)/', 'kong.views.test_object', name='kong_test_list'),
-     url(r'^tests/', 'kong.views.test_list', name='kong_testresult_list'),
-
-
-     url(r'^types/(?P<type_slug>.*?)/', 'kong.views.type_object', name='kong_type_detail'),
-     url(r'^types/', 'kong.views.type_list', name='kong_type_list'),
-
-     url(r'^servers/(?P<server_slug>.*?)/', 'kong.views.server_object', name='kong_server_detail'),
-     url(r'^servers/', 'kong.views.server_list', name='kong_server_list'),
-
 )
--- kong/views.py
+++ kong/views.py
@@ -20,77 +20,10 @@ def index(request):
                        {'results': ret_val},
                        context_instance=RequestContext(request))
 
-def site_list(request):
-    qs = Site.objects.all()
-    return list_detail.object_list(request, qs)
-
-def site_object(request, site_slug):
-    ret_val = []
-    sites = Site.objects.filter(slug=site_slug)
-    for site in sites:
-        results = get_latest_results(site)
-        ret_val.extend(results)
-
-    return render_to_response('kong/index.html',
-                       {'results': ret_val},
-                       context_instance=RequestContext(request))
-
-def test_list(request):
-    qs = Test.objects.all()
-    return list_detail.object_list(request, qs)
-
-def test_object(request, test_slug):
-    test = Test.objects.get(slug=test_slug)
-    tests = TestResult.objects.filter(test=test)
-    return render_to_response('kong/testresult_detail.html',
-                       {'results': tests },
-                       context_instance=RequestContext(request))
-
 def test_object_for_site(request, test_slug, site_slug):
     test = Test.objects.get(slug=test_slug)
     site = Site.objects.get(slug=site_slug)
     tests = TestResult.objects.filter(test=test, site=site)[:15]
     return render_to_response('kong/testresult_for_site.html',
                        {'results': tests },
-                       context_instance=RequestContext(request))
-
-def testresult_detail(request, id):
-    test = TestResult.objects.get(id=id)
-    template = test.test.render(test.site)
-    return render_to_response('kong/testresult_detail.html',
-                       {'result': test,
-                        'template': template, },
-                       context_instance=RequestContext(request))
-
-def type_list(request):
-    qs = Type.objects.all()
-    return list_detail.object_list(request, qs)
-
-def type_object(request, type_slug):
-    type = Type.objects.get(slug=type_slug)
-    sites = type.sites.all()
-    ret_val = []
-    for site in sites:
-        results = get_latest_results(site)
-        ret_val.extend(results)
-
-    return render_to_response('kong/index.html',
-                       {'results': ret_val },
-                       context_instance=RequestContext(request))
-
-
-def server_list(request):
-    qs = Server.objects.all()
-    return list_detail.object_list(request, qs)
-
-def server_object(request, server_slug):
-    server = Server.objects.get(slug=server_slug)
-    sites = server.sites.all()
-    ret_val = []
-    for site in sites:
-        results = get_latest_results(site)
-        ret_val.extend(results)
-
-    return render_to_response('kong/index.html',
-                       {'results': ret_val },
                        context_instance=RequestContext(request))
