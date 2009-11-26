f76aa7a6e5a0add70c31d8882b0f73d7acb36630
Eric Holscher <eric@ericholscher.com>
2009-11-26 05:24:06 +0000

    Updated views for use with Flot. Now returns data for graphing as well as just for dislaying

--- kong/views.py
+++ kong/views.py
@@ -6,24 +6,43 @@ from kong.models import Site, Type, Server
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.generic import list_detail
+import calendar
+
+def get_timestamp(time):
+    return calendar.timegm(time.timetuple()) / 100
+    #Need this for flot timestamps..
+    #return calendar.timegm(time.timetuple()) * 1000
 
 def index(request):
     ret_val = {}
+    flot_val = {}
     for site in Site.objects.all():
         results = get_latest_results(site)
         if ret_val.has_key(site.slug):
             ret_val[site.slug].extend(results)
         else:
             ret_val[site.slug] = results
+    for result in results:
+        results = list(TestResult.objects.filter(test=result.test, site=result.site)[:15])
+        results.reverse()
+        flot_val[result.test.slug] = [[get_timestamp(result.run_date), result.duration/1000] for result in results]
 
     return render_to_response('kong/index.html',
-                       {'results': ret_val},
+                       {'results': ret_val,
+                        'flot_list': flot_val},
                        context_instance=RequestContext(request))
 
 def test_object_for_site(request, test_slug, site_slug):
     test = Test.objects.get(slug=test_slug)
     site = Site.objects.get(slug=site_slug)
     tests = TestResult.objects.filter(test=test, site=site)[:15]
+    duration_tests = list(tests)
+    duration_tests.reverse()
+    duration_list = [result.duration for result in duration_tests]
+    flot_list = [[get_timestamp(result.run_date), result.duration/1000] for result in duration_tests]
     return render_to_response('kong/testresult_for_site.html',
-                       {'results': tests },
+                       {'results': tests,
+                        'duration_list': duration_list,
+                        'flot_list': flot_list
+                        },
                        context_instance=RequestContext(request))
