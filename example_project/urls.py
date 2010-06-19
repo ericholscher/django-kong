81e1f476a44fce89105f6249ccfff18675a1647a
eric <eric@Golem.(none)>
2010-06-19 02:26:05 +0000

    Fix urls up a bit.

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,21 +1,14 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
     (r'', include('kong.urls')),
+    (r'kong/', include('kong.urls')),
 
 )
 
