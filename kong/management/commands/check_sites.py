e420e33ffc5615e4348c4074ce409027f633f3d4
Eric Holscher <eric@ericholscher.com>
2010-09-23 13:40:24 -0500

    Added back the all-sites and all-sites to the management command.

--- kong/management/commands/check_sites.py
+++ kong/management/commands/check_sites.py
@@ -4,12 +4,16 @@ from kong.models import Site, Type
 from kong.utils import run_test, run_tests_for_type, run_tests_for_site, run_tests_for_box
 from optparse import OptionParser, make_option
 
+
+
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option("-t", "--test", dest="test"),
         make_option("-s", "--site", dest="site"),
         make_option("-T", "--type", dest="type"),
         make_option("-l", "--list", dest="list", action="store_true", default=False),
+        make_option("-a", "--all-sites", dest="all-sites", action="store_true"),
+        make_option("-A", "--all-types", dest="all-types", action="store_true"),
     )
 
     def handle(self, *args, **options):
@@ -17,6 +21,8 @@ class Command(BaseCommand):
         SITE = options.get('site')
         TYPE = options.get('type')
         LIST = options.get('list')
+        ALL_SITES = options.get('all-sites')
+        ALL_TYPES = options.get('all-types')
 
         passed =  True
         if TEST:
@@ -38,6 +44,12 @@ class Command(BaseCommand):
             print "All Tests:"
             for test in Test.objects.all():
                 print test.slug
+        elif ALL_SITES:
+            for site in Site.objects.all():
+                passed = run_tests_for_site(site)
+        elif ALL_TYPES:
+            for type in Type.objects.all():
+                passed = run_tests_for_type(type)
         else:
             print "No action"
             return 0
